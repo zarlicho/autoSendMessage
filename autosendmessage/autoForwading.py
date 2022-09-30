ee472d21f8e5bee348f5351dfef025d164821791
zarlicho <63176268+zarlicho@users.noreply.github.com>
2022-09-30 19:47:52 +0700

    updated

--- autosendmessage/autoForwading.py
+++ autosendmessage/autoForwading.py
@@ -10,9 +10,9 @@ from random import randint
 from time import sleep
 import csv
 colorama.init(autoreset=True)
-api_id = '17691195'
-api_hash = '5dc315d015c6b7907cf682756d7b6a4a'
-phone = '+6287879012895'
+api_id = '16093788'
+api_hash = '1f8acac96faeb96d4f095360e9f74760'
+phone = '+60146333803'
 client = TelegramClient(phone, api_id, api_hash)
 
 client.connect()
@@ -20,16 +20,9 @@ if not client.is_user_authorized():
     client.send_code_request(phone)
     client.sign_in(phone, input('Enter the code: '))
 
-chats = []
-last_date = None
-chunk_size = 200
 groups=[]
-bot = []
 i=0
 channel = "19375432"
-# get all channels
-channel_username = 'nessC'# your channel
-groupUsername = "JOSS ALERT"# your group
 psn = []
 
 while True:
@@ -47,7 +40,7 @@ while True:
                     i=0
                 print(Fore.LIGHTGREEN_EX+"[+] "+Fore.RESET+"Sending message to "+Fore.YELLOW+ group.title)
                 client.send_message(group.id, psn)
-                sleep(1)
+                sleep(randint(1,6))
             except Exception as e:
                 print(e)
                 
